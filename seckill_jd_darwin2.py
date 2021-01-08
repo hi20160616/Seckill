86d290131962339b5060012da643fe2684e177c9
hi20160616 <hi20160616@163.com>
2021-01-08 10:00:58 +0800

    update

--- seckill_jd_darwin2.py
+++ seckill_jd_darwin2.py
@@ -10,10 +10,14 @@ from selenium.webdriver.support import expected_conditions as EC
 # Key values
 watched_url = "https://yushou.jd.com/member/qualificationList.action"
 link_text = '飞天 53%vol 500ml 贵州茅台酒（带杯）'
-#  good_url = "https://item.jd.com/100012043978.html"
-kill_sec = '2021-01-08 10:00:00'
+kill_sec = '2021-01-06 10:00:00'
 chromedriver_path = r"./chromedriver"
 wait_login = 30
+
+# Test case
+#  watched_url = "https://search.jd.com/Search?keyword=%E8%8C%85%E5%8F%B0%E9%A3%9E%E5%A4%A9%2053%E5%BA%A6&enc=utf-8&suggest=5.def.0.base&wq=maotai&pvid=27d6a83f008c497885fc8de80c894dfa"
+#  link_text = '仅限正式PLUS会员预约享抢购资格，1月6日10:30开启预约，1月7日10:00开始抢购（京东APP需升级至8.5.6版本及以上）'
+#  kill_sec = '2021-01-06 10:00:00'
 
 options = webdriver.ChromeOptions()  # 配置 chrome 启动属性
 # 此步骤很重要，设置为开发者模式，防止被各大网站识别出来使用了Selenium
@@ -22,9 +26,8 @@ options.add_experimental_option("excludeSwitches", ['enable-automation'])
 # 懒加载模式变量准备，不等待页面加载完毕
 capa = DesiredCapabilities.CHROME
 capa["pageLoadStrategy"] = "none"  
-driver = webdriver.Chrome(executable_path=chromedriver_path, options=options, desired_capabilities=capa)
-#  driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
 
+driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
 
 wait = WebDriverWait(driver, 10)  # 超时时长为10s
 delay = timedelta.microseconds
@@ -38,6 +41,9 @@ def login():
 
 
 def buy(kill_time):
+    #  For test case
+    #  item_url = driver.find_element_by_xpath(
+    #      f'//*[@title="{link_text}"]').get_attribute('href')
     item_url = driver.find_element_by_link_text(
         link_text).get_attribute('href')
     while True:
@@ -45,6 +51,8 @@ def buy(kill_time):
         if kill_time <= datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]:
             a = datetime.now()
             #  启动懒加载，driver.get() will not be blocked.
+            webdriver.Chrome(executable_path=chromedriver_path,
+                             options=options, desired_capabilities=capa)
             driver.get(item_url)
             print("已尝试打开商品页!")
 
@@ -56,8 +64,8 @@ def buy(kill_time):
                 print("数量 增加 1 次")
                 wait.until(
                     EC.presence_of_element_located(
-                        (By.LINK_TEXT, "抢购"))
-                        #  (By.LINK_TEXT, "开始预约"))
+                        (By.LINK_TEXT, "开始预约"))
+                        #  (By.LINK_TEXT, "抢购"))
                 ).click()
                 print("点击 抢购 1 次")
                 b = datetime.now()
